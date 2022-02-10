fae2fe2b02d2e4863f0c79ab51ffabe1c5e16b0c
nishavaish <98401486+nishavaish@users.noreply.github.com>
2022-02-10 22:24:18 +0530

    updated url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://playgpbd.igpl.pro/';
+$config['base_url'] = 'http://game-env.eba-npkjfumf.ap-southeast-1.elasticbeanstalk.com/';
 
 
 /*
